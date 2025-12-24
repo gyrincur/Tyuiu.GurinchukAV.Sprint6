19398e20ec028155169bd109b191a0ec1660fcd7
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-12-24 11:26:30 +0300

    ee

--- Tyuiu.GurinchukAV.Sprint6.Task5.V26.Test/DataServiceTest.cs
+++ Tyuiu.GurinchukAV.Sprint6.Task5.V26.Test/DataServiceTest.cs
@@ -8,6 +8,11 @@ namespace Tyuiu.GurinchukAV.Sprint6.Task5.V26.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\DataSprint5\Sprint5Task5\Sprint5Task5\InPutDataFileTask5V26.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
--- Tyuiu.GurinchukAV.Sprint6.Task6.V2.Test/DataServiceTest.cs
+++ Tyuiu.GurinchukAV.Sprint6.Task6.V2.Test/DataServiceTest.cs
@@ -2,11 +2,16 @@
 namespace Tyuiu.GurinchukAV.Sprint6.Task6.V2.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\DataSprint5\Sprint5Task6\Sprint5Task6\InPutDataFileTask6V2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
